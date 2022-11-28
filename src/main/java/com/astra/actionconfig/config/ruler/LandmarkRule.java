459741608b888a8d415309f63c451fbe6bc734c8
yiwei <yiwei@orbbec.com>
2022-11-28 14:03:23 +0800

    　add landmark rule satisfy

--- src/main/java/com/astra/actionconfig/config/ruler/LandmarkRule.java
+++ src/main/java/com/astra/actionconfig/config/ruler/LandmarkRule.java
@@ -9,31 +9,12 @@ import com.astra.actionconfig.config.ruler.landmarkd.AngleToLandmark;
 import com.astra.actionconfig.config.ruler.landmarkd.DistanceToLandmark;
 import com.astra.actionconfig.config.ruler.landmarkd.LandmarkToStateAngle;
 import com.astra.actionconfig.config.ruler.landmarkd.LandmarkToStateDistance;
-import jdk.nashorn.internal.runtime.options.Option;
 import lombok.Data;
 
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-
-
-class RuleSatisfyData {
-    public boolean satisfy;
-    public Set<Warning> warnings;
-    public int pass;
-    public int total;
-
-    public RuleSatisfyData(boolean satisfy, HashSet<Warning> warnings, int pass, int total) {
-        this.satisfy = satisfy;
-        this.warnings = warnings;
-        this.pass = pass;
-        this.total = total;
-
-    }
-}
-
-
 
 
 @Data
@@ -51,10 +32,133 @@ public class LandmarkRule {
                                       Map<LandmarkType, Point3F> poseMap,
                                       List<Observation> objects,
                                       Point2F frameSize) {
+        RuleSatisfyData distanceToLandmarkSatisfies =
+                distanceToLandmark.stream().reduce(
+                        new RuleSatisfyData(true,
+                                new HashSet<Warning>(),
+                                0, 0),
+                        (result, next) -> {
+                            boolean satisfy = next.satisfy(poseMap);
+                            Set<Warning> newWarnings = result.warnings;
 
+                            if (next.warning.triggeredWhenRuleMet && satisfy) {
+                                newWarnings.add(next.warning);
+                            }else if (!next.warning.triggeredWhenRuleMet && !satisfy) {
+                                newWarnings.add(next.warning);
+                            }
 
+                            return new RuleSatisfyData(
+                                    result.satisfy && satisfy,
+                                    newWarnings,
+                                    satisfy ? result.pass + 1 : result.pass,
+                                    result.total + 1
+                            );
+                        }, (a, b) -> {
+                            return  null;
+                        }
 
-        return new RuleSatisfyData(false, new HashSet<Warning>(), 0 ,0);
+                );
+
+        RuleSatisfyData angleToLandmarkSatisfies =
+                angleToLandmark.stream().reduce(
+                        new RuleSatisfyData(true,
+                                new HashSet<Warning>(),
+                                0, 0),
+                        (result, next) -> {
+                            boolean satisfy = next.satisfy(poseMap);
+                            Set<Warning> newWarnings = result.warnings;
+
+                            if (next.warning.triggeredWhenRuleMet && satisfy) {
+                                newWarnings.add(next.warning);
+                            }else if (!next.warning.triggeredWhenRuleMet && !satisfy) {
+                                newWarnings.add(next.warning);
+                            }
+
+                            return new RuleSatisfyData(
+                                    result.satisfy && satisfy,
+                                    newWarnings,
+                                    satisfy ? result.pass + 1 : result.pass,
+                                    result.total + 1
+                            );
+                        }, (a, b) -> {
+                            return  null;
+                        }
+
+                );
+
+        RuleSatisfyData landmarkToStateDistanceSatisfies =
+                landmarkToStateDistance.stream().reduce(
+                        new RuleSatisfyData(true,
+                                new HashSet<Warning>(),
+                                0, 0),
+                        (result, next) -> {
+                            boolean satisfy = next.satisfy(stateTimeHistory, poseMap);
+                            Set<Warning> newWarnings = result.warnings;
+
+                            if (next.warning.triggeredWhenRuleMet && satisfy) {
+                                newWarnings.add(next.warning);
+                            }else if (!next.warning.triggeredWhenRuleMet && !satisfy) {
+                                newWarnings.add(next.warning);
+                            }
+
+                            return new RuleSatisfyData(
+                                    result.satisfy && satisfy,
+                                    newWarnings,
+                                    satisfy ? result.pass + 1 : result.pass,
+                                    result.total + 1
+                            );
+                        }, (a, b) -> {
+                            return  null;
+                        }
+
+                );
+
+        RuleSatisfyData landmarkToStateAngleSatisfies =
+                landmarkToStateAngle.stream().reduce(
+                        new RuleSatisfyData(true,
+                                new HashSet<Warning>(),
+                                0, 0),
+                        (result, next) -> {
+                            boolean satisfy = next.satisfy(stateTimeHistory, poseMap);
+                            Set<Warning> newWarnings = result.warnings;
+
+                            if (next.warning.triggeredWhenRuleMet && satisfy) {
+                                newWarnings.add(next.warning);
+                            }else if (!next.warning.triggeredWhenRuleMet && !satisfy) {
+                                newWarnings.add(next.warning);
+                            }
+
+                            return new RuleSatisfyData(
+                                    result.satisfy && satisfy,
+                                    newWarnings,
+                                    satisfy ? result.pass + 1 : result.pass,
+                                    result.total + 1
+                            );
+                        }, (a, b) -> {
+                            return  null;
+                        }
+
+                );
+
+        Set<Warning> warnings = new HashSet<Warning>();
+        warnings.addAll(distanceToLandmarkSatisfies.warnings);
+        warnings.addAll(angleToLandmarkSatisfies.warnings);
+        warnings.addAll(landmarkToStateDistanceSatisfies.warnings);
+        warnings.addAll(landmarkToStateAngleSatisfies.warnings);
+        return new RuleSatisfyData(
+                distanceToLandmarkSatisfies.satisfy &&
+                        angleToLandmarkSatisfies.satisfy &&
+                landmarkToStateDistanceSatisfies.satisfy &&
+                landmarkToStateAngleSatisfies.satisfy,
+                warnings,
+                distanceToLandmarkSatisfies.pass +
+                        angleToLandmarkSatisfies.pass +
+                        landmarkToStateDistanceSatisfies.pass +
+                        landmarkToStateAngleSatisfies.pass,
+                distanceToLandmarkSatisfies.total +
+                        angleToLandmarkSatisfies.total +
+                        landmarkToStateDistanceSatisfies.total +
+                        landmarkToStateAngleSatisfies.total);
     }
 
 
---
+++ src/main/java/com/astra/actionconfig/config/ruler/RuleSatisfyData.java
@@ -0,0 +1,21 @@
+package com.astra.actionconfig.config.ruler;
+
+import com.astra.actionconfig.config.data.Warning;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class RuleSatisfyData {
+    public boolean satisfy;
+    public Set<Warning> warnings;
+    public int pass;
+    public int total;
+
+    public RuleSatisfyData(boolean satisfy, Set<Warning> warnings, int pass, int total) {
+        this.satisfy = satisfy;
+        this.warnings = warnings;
+        this.pass = pass;
+        this.total = total;
+
+    }
+}
